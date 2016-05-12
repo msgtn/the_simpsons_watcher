0402dabc182cf098423da1f6328b7b5d3c97889d
Michael Suguitan <psychomugs@gmail.com>
2016-05-12 00:38:45 -0400

    Automatically plays the first episode' '

--- the_simpsons_rand.py
+++ the_simpsons_rand.py
@@ -1,7 +1,25 @@
 import selenium
 from selenium import webdriver as wd
+import time
 
 b = wd.Firefox()
-b.get('http://www.watch-episodes.tv/the-simpsons')
-season_links = b.find_elements_by_class_name('sn-button')
-episode_links = b.find_elements_by_class_name('season')
+# b.get('http://www.watch-episodes.tv/the-simpsons')
+# season_links = b.find_elements_by_class_name('sn-button')
+# episode_links = b.find_elements_by_class_name('season')
+
+b.get('http://watchcartoonsonline.eu/watch-the-simpsons-season-1-full-episodes-online-free/')
+time.sleep(20)
+while(1):
+    try:
+        b.find_element_by_class_name('spu-icon-close').click()
+        break
+    except:
+        pass
+        
+
+ep_links = b.find_elements_by_partial_link_text('The Simpsons Season')
+ep_links[1].click()
+
+b.switch_to_window(b.window_handles[1])
+b.find_elements_by_class_name('jw-icon-display')[0].click()
+
